d6b2c7c94076c96acf69bfdfec62c3699817aa8f
Grig Betsan <griggors@gmail.com>
2018-08-22 15:59:07 +0300

    Syncing

--- tbot.cr
+++ tbot.cr
@@ -10,5 +10,18 @@ require "totem"
 require "./src/main"
 
 puts "Initialize"
-bot = TBot::Bot.new(TBot::Config::BOT_NAME, TBot::Config::BOT_KEY)
-bot.polling
+
+include TBot::Helpers
+chat_id = "-1001197398635"
+json = JSON.parse(File.read("users.json")).as_h
+json.each do |user_id, username|
+  next if username == "None"
+  if user_exists?(user_id.to_i, chat_id) 
+    User.create_from(user_id.to_i, chat_id, username.as_s)
+  end
+end
+
+
+
+#bot = TBot::Bot.new(TBot::Config::BOT_NAME, TBot::Config::BOT_user_idEY)
+#bot.polling
