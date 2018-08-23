9fbc6aa1244848d5f93df834dd0ed7e11acf9808
Grig Betsan <griggors@gmail.com>
2018-08-23 23:16:38 +0300

    Fix syncing

--- tbot.cr
+++ tbot.cr
@@ -16,7 +16,7 @@ chat_id = "-1001197398635"
 json = JSON.parse(File.read("users.json")).as_h
 json.each do |user_id, username|
   next if username == "None"
-  if user_exists?(user_id.to_i, chat_id) 
+  if !user_exists?(user_id.to_i, chat_id) 
     User.create_from(user_id.to_i, chat_id, username.as_s)
   end
 end
