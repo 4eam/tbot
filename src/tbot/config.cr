e41d37ab888cb46775ce8cacfebf341252391b91
Grig Betsan <griggors@gmail.com>
2018-08-26 22:38:46 +0300

    Change config name

--- src/tbot/config.cr
+++ src/tbot/config.cr
@@ -1,5 +1,5 @@
 struct TBot::Config
-  TOTEM       = Totem.from_file("./config.yaml")
+  TOTEM       = Totem.from_file("./.tbot.yml")
   BOT_KEY     = TOTEM.get("bot_api_key").as_s
   BOT_NAME    = TOTEM.get("bot_name").as_s
   DB_LOCATION = TOTEM.get("database.file").as_s
